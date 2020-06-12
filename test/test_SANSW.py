82b8aaf3dacca9bdcbac8cff7849debdd472ddc3
zane <zane.chen@feixitek.com>
2020-06-12 14:06:51 +0800

    Zane:1.test_Sendemail.py Add more assert；2.Add test_SANSW.py；

---
+++ test/test_SANSW.py
@@ -0,0 +1,141 @@
+import pytest
+import io
+import sys
+import datetime
+import SANSW as sw
+import GetConfig as gc
+try:
+    import configparser as cp
+except Exception:
+    import ConfigParser as cp
+
+
+cfg = cp.ConfigParser(allow_no_value=True)
+cfg.read('config.ini')
+ip = str(cfg.get('SANSwitches', 'switch0'))
+ssh_port = cfg.getint('SANSwitcheSetting', 'ssh_port')
+user = str(cfg.get('SANSwitcheSetting', 'username'))
+pw = str(cfg.get('SANSwitcheSetting', 'password'))
+port = cfg.get('SANSwitchePorts', 'switch0')
+
+sys_cfg = cp.ConfigParser(allow_no_value=True)
+sys_cfg.read('sys_cfg.ini')
+ssc = list(i[0] for i in sys_cfg.items('PCSANSwitchCommand'))
+pcfolder = sys_cfg.get('FolderSetting', 'PeriodicCheck')
+fname = 'PC_%s_SANSwitch_%s.log' % (
+    datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'), ip)
+
+
+def test_clear_all():
+    assert sw.clear_all() == None
+
+
+def test_clear_one_port():
+    assert sw.clear_one_port(ip, ssh_port) == None
+
+
+def test_print_porterror_all_formated():
+    assert sw.print_porterror_all_formated() == None
+
+
+def test_print_porterror_formated():
+    assert sw.print_porterror_formated(ip) == None
+
+
+def test_print_switchshow_all():
+    assert sw.print_switchshow_all() == None
+
+
+def test_print_switchshow():
+    assert sw.print_switchshow(ip) == None
+
+
+def test_periodically_check_all():
+    assert sw.periodically_check_all() == None
+
+
+def test_periodically_check():
+    assert sw.periodically_check(ip) == None
+
+
+def test_get_info_for_DB():
+    a = []
+    for i in sw.get_info_for_DB():
+        a.append(i)
+    assert isinstance(a[0], dict)
+    assert isinstance(a[1], dict)
+    assert isinstance(a[2], dict)
+
+
+class TestAction():
+
+    def setup_class(self):
+        self.act = sw.Action(ip, ssh_port, user, pw, [])
+
+    def test_get_switch_info(self):
+        assert self.act._get_switch_info() == True
+
+    def test_print_porterrshow(self):
+        assert self.act.print_porterrshow() == None
+
+    def test_print_switchshow(self):
+        assert self.act.print_switchshow() == None
+
+    def test_clear_all_port(self):
+        sys.stdout = io.BytesIO()
+        assert self.act.clear_all_port() == None
+        assert 'completed' in sys.stdout.getvalue()
+
+    def test_clear_one_port(self):
+        assert self.act.clear_one_port(port) == True
+
+    def test_periodic_check(self):
+        assert self.act.periodic_check(ssc, pcfolder, fname) == None
+
+
+class TestStatus():
+
+    def setup_class(self):
+        self.st = sw.Status(ip, ssh_port, user, pw, [1, 2, 3, 4, 5, 6])
+
+    def test_PutErrorToDict(self):
+        assert self.st._dicPartPortError is not None
+
+    def test_err_num_int(self):
+        assert self.st.err_num_int('259') == 259
+        assert self.st.err_num_int('2.5k') == 2500
+        assert self.st.err_num_int('3.8m') == 3800000
+        assert self.st.err_num_int('6.76g') == 6760000000
+
+    def test_list_string_to_int(self):
+        date = ['2.9k', '12', '3.4m', '333', '5.9g', '22k', '2']
+        assert self.st.list_string_to_int(
+            date) == [2900, 12, 3400000, 333, 5900000000, 22000, 2]
+
+    def test_dict_string_to_int(self):
+        assert self.st._dict_string_to_int(self.st._dicPartPortError) == None
+
+    def test_sum_and_total(self):
+        assert self.st.sum_and_total() is not None
+
+    def test_print_porterror_formated(self):
+        assert self.st.print_porterror_formated() == None
+
+
+class TestInfoForDB():
+
+    def setup_class(self):
+        self.infodb = sw.InfoForDB('switch0', '10.203.1.9', [1, 2, 3, 4, 5, 6])
+
+    def test_get_dicOrigin(self):
+        date = self.infodb.get_dicOrigin()
+        assert isinstance(date, dict)
+
+    def test_get_dicPEFormated(self):
+        date = self.infodb.get_dicPEFormated()
+        assert isinstance(date, dict)
+
+    def test_get_summary_total(self):
+        date = self.infodb.get_summary_total()
+        PE_Sum = date['switch0']['PE_Sum']
+        assert isinstance(PE_Sum, list)
--- test/test_Sendemail.py
+++ test/test_Sendemail.py
@@ -1,37 +1,51 @@
 import pytest
 import SendEmail
+import io
+import sys
 
-E = SendEmail.Email()
 
 class TestEmail():
 
+    def setup_class(self):
+        self.E = SendEmail.Email()
+
     def test_get_msg(self):
-        assert E.get_msg('pytest', 'test')
+        assert self.E.get_msg('pytest', 'test')
 
     def test_prepare(self):
-        assert E.prepare()
+        assert self.E.prepare()
 
     def test_anonymous_prepare(self):
-        assert E.anonymous_prepare()
+        assert self.E.anonymous_prepare()
 
     def test_send_email(self):
-    	assert E.send_email('pytest', 'test') == None
+        assert self.E.send_email('pytest', 'test') == None
 
 
 def testfunc():
-	return
+    return
+
 
 def test_switch():
-	switch = SendEmail.email_switch(testfunc())
-	assert switch
+    switch = SendEmail.email_switch(testfunc())
+    assert switch
 
 
 def test_send_warnmail():
-	info = [['2020-06-01 11:36:42', '10.203.1.4', 'engine1', 2, 'Engine reboot 6674 secends ago']]
-	assert SendEmail.send_warnmail(info) == None
+    info = [['2020-06-01 11:36:42', '10.203.1.4',
+             'engine1', 2, 'Engine reboot 6674 secends ago']]
+    sys.stdout = io.BytesIO()
+    assert SendEmail.send_warnmail(info) == None
+    assert sys.stdout.getvalue() == 'Send success!\n'
+
 
 def test_send_test():
-	assert SendEmail.send_test() == None
+    sys.stdout = io.BytesIO()
+    assert SendEmail.send_test() == None
+    assert sys.stdout.getvalue() == 'Send success!\n'
+
 
 def test_send_live():
-	assert SendEmail.send_live() == None
+    sys.stdout = io.BytesIO()
+    assert SendEmail.send_live() == None
+    assert sys.stdout.getvalue() == 'Send success!\n'
